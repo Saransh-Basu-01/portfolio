82c486f82863799b11467020e05436e406f78b15
Saransh-Basu-01 <saranshbasu@gmail.com>
2025-08-25 11:11:56 +0545

    added

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -102,6 +102,7 @@ const AboutSection = () => {
                 'Next.js',
                 'Tailwind CSS',
                 'FastAPI',
+                'Flask',
                 'Git',
                 'PostgreSQL',
                 'MySQL',
